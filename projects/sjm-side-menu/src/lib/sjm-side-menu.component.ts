53f6b19d9e99ae00e8d189060abf08aad3e326f0
Samys Molina <samyjes4@gmail.com>
2024-12-03 12:27:15 -0400

    add enum in sjm-side-menu

--- projects/sjm-side-menu/src/lib/sjm-side-menu.component.ts
+++ projects/sjm-side-menu/src/lib/sjm-side-menu.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
+export enum TitleColor {
+  red = 'text-red-500',
+  green = 'text-green-500',
+  blue = 'text-blue-500',
+  purple = 'text-purple-500',
+}
 @Component({
   selector: 'lib-sjm-side-menu',
-  imports: [],
-  template: `
-    <p>
-      sjm-side-menu works!
-    </p>
-  `,
+  imports: [RouterLink, RouterLinkActive,],
+  templateUrl: './sjm-side-menu.component.html',
   styles: ``
 })
 export class SjmSideMenuComponent {
+
+  isAuthenticated = input(false);
+
+  titleColor = input<TitleColor>(TitleColor.purple)
+
+  onSignOut = output();
+  onSignIn = output();
+
+
+
 
 }
--- projects/sjm-testbed-app/src/app/app.component.ts
+++ projects/sjm-testbed-app/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {SjmSideMenuComponent} from 'sjm-side-menu'
+import { SjmSideMenuComponent, TitleColor } from "../../../sjm-side-menu/src/lib/sjm-side-menu.component";
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,8 @@ import {SjmSideMenuComponent} from 'sjm-side-menu'
 })
 export class AppComponent {
   title = 'sjm-testbed-app';
+
+
+  TitleColor = TitleColor;
+  isAuthenticated = signal(true);
 }
