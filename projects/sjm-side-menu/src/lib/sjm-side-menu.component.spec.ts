60c1ada642dc016709a2844bf5db57c2f4a5b044
Samys Molina <samyjes4@gmail.com>
2024-12-03 15:09:34 -0400

    testing login and logout

--- projects/sjm-side-menu/src/lib/sjm-side-menu.component.spec.ts
+++ projects/sjm-side-menu/src/lib/sjm-side-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SjmSideMenuComponent } from './sjm-side-menu.component';
+import { provideRouter } from '@angular/router';
 
 describe('SjmSideMenuComponent', () => {
   let component: SjmSideMenuComponent;
@@ -8,7 +9,10 @@ describe('SjmSideMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SjmSideMenuComponent]
+      imports: [SjmSideMenuComponent,],
+      providers: [
+        provideRouter([])
+      ]
     })
     .compileComponents();
 
@@ -20,4 +24,35 @@ describe('SjmSideMenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call onSignIn when button is clicked', () => {
+    
+    spyOn(component.onSignIn, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', false);
+
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('[data-login]') as HTMLButtonElement;
+
+    button.click();
+
+    expect(component.onSignIn.emit).toHaveBeenCalled();
+
+  })
+
+  it('should call onSignOut when button is clicked', () => {
+    
+    spyOn(component.onSignOut, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', true);
+
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('[data-logout]') as HTMLButtonElement;
+
+    expect(button).toBeTruthy();
+
+    button.click();
+
+    expect(component.onSignOut.emit).toHaveBeenCalled();
+
+  })
+
 });
